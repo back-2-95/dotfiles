ea3d222614f218f38a02c392e6391c058ed0deb8
Marko Korhonen <marko.korhonen@druid.fi>
2020-03-07 16:53:27 +0200

    README.md and try to install ohmyzah unattended

---
+++ README.md
@@ -0,0 +1,22 @@
+# dotfiles
+
+These are my dotfiles
+
+## Requirements
+
+- Clean install of macOS Catalina
+- Xcode Command Line Tools (`xcode-select --install`)
+
+## Setting up your Mac
+
+Clone this repository to `~/dotfiles`:
+
+```
+$ git clone https://github.com/back-2-95/dotfiles.git ~/dotfiles
+```
+
+Start the setup:
+
+```
+$ cd ~/dotfiles && source install.sh
+```
--- install.sh
+++ install.sh
@@ -14,35 +14,35 @@ step () {
 step "Setting up your Mac\n"
 
 # Check for Xcode Command Line Tools and install if we don't have it
-step "Installing Xcode Command Line Tools"
-if [ -d "$(xcode-select -p)" ]; then
-  printf "Xcode Command Line Tools is already installed\n"
-else
-  xcode-select --install
-fi
+#step "Installing Xcode Command Line Tools"
+#if [ -d "$(xcode-select -p)" ]; then
+#  printf "Xcode Command Line Tools is already installed\n"
+#else
+#  xcode-select --install
+#fi
 
 # Check for Oh My Zsh and install if we don't have it
-step "Installing Oh My Zsh"
+step "Installing Oh My Zsh ..."
 if [ -d "$HOME/.oh-my-zsh" ]; then
   printf "Oh My Zsh is already installed\n"
 else
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # Check for Homebrew and install if we don't have it
-step "Installing Homebrew"
+step "Installing Homebrew ..."
 if [ -x "$(which brew)" ]; then
   printf "Homebrew is already installed\n"
 
   # Update Homebrew recipes
-  step "Update Homebrew"
+  step "Updating Homebrew ..."
   brew update
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install all our dependencies with bundle (See Brewfile)
-step "Install Homebrew Bundle"
+step "Installing Homebrew Bundle ..."
 brew bundle
 
 step "Symlink .zshrc $HOME"
@@ -54,5 +54,5 @@ ln -sfn $HOME/dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
-step "Set macOS preferences"
+step "Set macOS preferences ..."
 source .macos
