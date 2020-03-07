86673c4eb72fe84c45cdfbc0e6a5067b0e22fc4b
Marko Korhonen <marko.korhonen@druid.fi>
2020-03-07 16:39:14 +0200

    Fix checks

--- install.sh
+++ install.sh
@@ -13,25 +13,33 @@ step () {
 
 step "Setting up your Mac\n"
 
+# Check for Xcode Command Line Tools and install if we don't have it
+step "Installing Xcode Command Line Tools"
+if [ -d "$(xcode-select -p)" ]; then
+  printf "Xcode Command Line Tools is already installed\n"
+else
+  xcode-select --install
+fi
+
 # Check for Oh My Zsh and install if we don't have it
 step "Installing Oh My Zsh"
-if [ -d "~/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
+if [ -d "$HOME/.oh-my-zsh" ]; then
   printf "Oh My Zsh is already installed\n"
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Check for Homebrew and install if we don't have it
 step "Installing Homebrew"
 if [ -x "$(which brew)" ]; then
   printf "Homebrew is already installed\n"
+
+  # Update Homebrew recipes
+  step "Update Homebrew"
+  brew update
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-
-# Update Homebrew recipes
-step "Update Homebrew"
-brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 step "Install Homebrew Bundle"
